703f515c0e84bb8eca3d149b3b03b95ae2575ce0
uzaimaoffical <uzaimabari2004@gmail.com>
2024-08-25 00:11:47 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,205 @@
+// let myNumber = [34,45,67,98,65,73];
+
+// for(let i=0;i<myNumber.length; i++){
+//     console.log(myNumber[i])
+// }
+// let marks =[84,96,74,63,52,36]
+// let sum =0;
+// for(let val of marks)
+//    {sum+=val}
+// console.log(sum);
+// let avg =sum / marks.length
+// console.log(avg);
+
+// let item= [250,300,350,400,450,500];
+// for(let i=0;i<item.length;i++){
+// let offer =item [i]/ 10;
+// item[i] -=offer
+
+// }
+//    console.log(item )
+//let price=[300,400,500,700,200];
+//console.log(price);
+// for(let i=0; i<price.length;i++){
+
+// let off=price[i] / 50;
+// price[i] -=off
+// }
+// console.log(price);
+// let company = ["microsoft","goole","uber","daraz"]
+// company.shift();
+// company
+// console.log(company);
+// company.splice(1,1,"ola");
+// console.log(company);
+// company.push("amazon");
+// console.log(company);
+
+// chapter 2
+
+// question 1
+
+// var userName = "bari";
+// alert(userName)
+//question 2
+// var fullName= "uzaima ali bari"
+// console.log(fullName);
+
+//question 02
+
+// let number1 = +prompt("enter your number1")
+// let number2 = +prompt("enter your number2")
+// if(number1 >number2){
+//     console.log(number1 + "is larger");
+// }else if (number1<number2){
+//     console.log(number2 + "is larger");
+// }
+
+//question 03
+
+// let userNumber = +prompt("enter a number ");
+// if(userNumber==0){
+//     console.log(userNumber + "zero");
+// } else if (userNumber>0){
+//     console.log(userNumber + "positive");
+// }else if (userNumber<0){
+//     console.log(userNumber + "negative");
+// }
+
+//question 4
+// let user =prompt("enter your vowel")
+// if(user == "a"){
+//     alert("vowel")
+// }else if (user =="e"){
+//     alert("vowel")
+
+// }else if (user == "i"){   alert("vowel")}
+// else if (user == "o"){   alert("vowel")}
+// else if (user == "u"){   alert("vowel")}
+//question 5
+// let pasward = prompt("Enter your pasward");
+// let conformpasward =prompt("Enter Your conformpasward");
+
+//      if(pasward =="")
+// {p
+//     alert("again")
+
+// }else if (pasward===conformpasward){
+//     alert("correct")}
+// else{
+//     alert("incorrect")
+// }
+
+// let name = ["uzaima", "ubaid", "saqib"]
+// let score = [124, 223, 324]
+
+// let arryjoin=["this ","is","book"]
+// let newArry =arryjoin.join("  ")
+// document.write(newArry)
+// function myfunction(num1,num2){
+//     let result =num1+num2
+//     console.log(result);
+//     return "uzaima"
+// }
+// myfunction(12,34)
+// let arr =[200,250,300,202,500];
+
+//     let max =arr[0];
+// for(let i =0; i<arr.length;i++){
+// if (max<arr[i]){
+//     max=arr[i]
+// }
+// }
+// console.log(max);
+// let paragraph=" uzaima ali bari"
+// paragraph[0].toUpperCase()+ paragraph.substring(1);
+// console.log(paragraph);
+
+// let number = 750;
+// let userInput = +prompt("Enter Your Exam Number");
+// let percentage=userInput / number*100;
+// console.log(percentage);
+
+// switch(true){
+//     case (percentage>=80 && percentage<100):
+//         alert("A-one");
+//         break
+//         case (percentage>=70 && percentage<80):
+//             alert("A");
+//             break
+//             case (percentage>=60 && percentage<70):
+//                 alert("B");
+//                 break
+//                 case (percentage>=50 && percentage<60):
+//                     alert("c");
+//                     break
+//                     case (percentage>40 && percentage<50):
+//                         alert("D");
+//                         break
+                        
+//                         case (percentage>=33 ):
+//                         alert("Fail");  
+//                         break      
+//                         case (percentage>=0):
+//                             alert("Fail");
+//                                      break
+// }
+
+// let largerNumber =[100 ,200 , 300, 400];
+// let max =[0]
+// for (let i = 0; i<largerNumber.length ;i++){
+//     if ( max < largerNumber[i]){
+//         max= largerNumber[i]
+//     }
+// }
+// console.log(max);
+
+// for(let i=0;i<5;i++){
+//     console.log(i);
+    
+// }
+// let i= 0
+// while(i<5){
+//     i++
+//     console.log(i);
+    
+// }
+// let j= 0
+
+
+// do {
+//     j++
+//     console.log(j);
+    
+// } while (j<5);
+
+// var str = "Hello World";
+// str[0] = "F";
+// console.log(str);
+// var number = 7.1
+// console.log(Math.floor(number));
+// function myfunction(a,b){
+// console.log(a+b);
+// return 
+
+
+// }
+// myfunction(1,3)
+// let a =12
+// console.log(toString(),a);
+// let b =(a.toFixed())
+// console.log(b);
+// var date =new Date() .getDay()
+// 1 var d = new Date();
+// 2 var b=.setFullYear(2001);
+
+// console.log(b);
+// function a (a,b){
+//     // var sum = 2+2
+//     console.log(a+b);
+    
+//     return sum;
+// }
+// a (12,3)
+
+
